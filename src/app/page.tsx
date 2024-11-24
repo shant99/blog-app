e2a887914ac923ff93488b98505c3b582df2fea5
shant99 <xxxx.shant.xxxx@gmail.com>
2024-11-24 16:51:34 +0400

    STL-004

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        Abrakatabra
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
