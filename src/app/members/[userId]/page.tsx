36f1bb286223bd496fc8c9930158faa589cfe2b3
shant99 <xxxx.shant.xxxx@gmail.com>
2024-12-11 02:43:12 +0400

    STL-014.3 Fixed bugs

--- src/app/members/[userId]/page.tsx
+++ src/app/members/[userId]/page.tsx
@@ -1,6 +1,6 @@
 import { getMemberByUserId } from "@/actions/members/getMemberByUserId";
 import { CardBody, CardHeader, Divider } from "@nextui-org/react";
-import { getMessages } from "next-intl/server";
+// import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -11,14 +11,14 @@ export default async function MemberDetailedPage({
 }) {
   const { userId } = await params;
   const member = await getMemberByUserId(userId);
-  const t = (await getMessages()) || {};
+  // const t = (await getMessages()) || {};
 
   if (!member) return notFound();
 
   return (
     <>
       <CardHeader className="text-2xl font-semibold text-default">
-        {t.user_profile.profile}
+        Profile
       </CardHeader>
       <Divider />
       <CardBody>{member.description || "No description"}</CardBody>
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,20 +2,16 @@
 
 import { getMembers } from "@/actions/members/getMembers";
 import MemberCard from "@/components/pageContents/MembersContent/MembersCard";
-import { getMessages } from "next-intl/server";
+// import { getMessages } from "next-intl/server";
 
 export default async function MembersPage() {
-  const t = await getMessages();
+  // const t = await getMessages();
   const members = await getMembers();
   const total = members?.length || 0;
 
   return (
     <div>
-      {members && (
-        <h2>
-          {t.members.count}: {total}
-        </h2>
-      )}
+      {members && <h2>Total ----: {total}</h2>}
       <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8 max-w-[1200px]">
         {members &&
           members.map((member) => (
