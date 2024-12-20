c52222a8ba314136fa24ee0a46af04b3e567cbf4
shant99 <xxxx.shant.xxxx@gmail.com>
2024-12-21 03:06:46 +0400

    STL-016.1 Fixed typescript on [userId] photos page

--- src/app/members/[userId]/photos/page.tsx
+++ src/app/members/[userId]/photos/page.tsx
@@ -5,9 +5,10 @@ import React from "react";
 export default async function PhotosPage({
   params,
 }: {
-  params: { userId: string };
+  params: Promise<{ userId: string }>;
 }) {
-  const photos = await getMemberPhotosByUserId(params.userId);
+  const { userId } = await params;
+  const photos = await getMemberPhotosByUserId(userId);
   return (
     <>
       <CardHeader className="text-2xl font-semibold text-default">
