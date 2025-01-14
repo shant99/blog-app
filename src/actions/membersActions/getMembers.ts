57ee7343fec18a5f749e56497a1a5e042ab6b96c
shant99 <xxxx.shant.xxxx@gmail.com>
2025-01-15 03:19:20 +0400

    STL-021 Added pagination

--- src/actions/membersActions/getMembers.ts
+++ src/actions/membersActions/getMembers.ts
@@ -1,20 +1,58 @@
 "use server";
 
-import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
+import { GetMemberParams, PaginatedResponse } from "@/types";
+import getAuthUserId from "../authActions/getAuthUserId";
+import { Member } from "@prisma/client";
+import getAgeRange from "@/utils/dates/getAgeRange";
 
-export async function getMembers() {
-  const session = await auth();
-  if (!session?.user) return null;
+export async function getMembers({
+  ageRange = "18,100",
+  gender = "male,female",
+  orderBy = "updated",
+  pageNumber = "1",
+  pageSize = "12",
+  withPhoto = "true",
+}: GetMemberParams): Promise<PaginatedResponse<Member>> {
+  const userId = await getAuthUserId();
+
+  const [minDob, maxDob] = getAgeRange(ageRange);
+
+  const selectedGender = gender.split(",");
+
+  const page = parseInt(pageNumber);
+  const limit = parseInt(pageSize);
+
+  const skip = (page - 1) * limit;
 
   try {
-    return prisma.member.findMany({
+    const membersSelect = {
       where: {
+        AND: [
+          { dateOfBirth: { gte: minDob } },
+          { dateOfBirth: { lte: maxDob } },
+          { gender: { in: selectedGender } },
+          ...(withPhoto === "true" ? [{ image: { not: null } }] : []),
+        ],
         NOT: {
-          userId: session.user.id,
+          userId,
         },
       },
+    };
+
+    const count = await prisma.member.count(membersSelect);
+
+    const members = await prisma.member.findMany({
+      ...membersSelect,
+      orderBy: { [orderBy]: "desc" },
+      skip,
+      take: limit,
     });
+
+    return {
+      items: members,
+      totalCount: count,
+    };
   } catch (error) {
     console.log(error);
     throw error;
